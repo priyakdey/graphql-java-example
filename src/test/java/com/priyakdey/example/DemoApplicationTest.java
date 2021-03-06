efdb2584933e39d9f82188c2423e5ce244f01fe9
Priyak Dey <priyak1991dey@gmail.com>
2021-03-06 20:34:13 +0530

    Initial Commit

---
+++ src/test/java/com/priyakdey/example/DemoApplicationTest.java
@@ -0,0 +1,13 @@
+package com.priyakdey.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTest {
+
+    @Test
+    void contextLoadsUp() {
+
+    }
+}
